bc6158bb2295f37e9222480cc1b131c7e9fe49b6
Gilles Hemmerlé <gilles.h@iabsis.com>
2016-03-01 15:12:12 +0100

    * PSR2 update

--- modules/ServerMgmt/controllers/PostfixController.php
+++ modules/ServerMgmt/controllers/PostfixController.php
@@ -1,84 +1,83 @@
 <?php
 
 namespace Igestis\Modules\ServerMgmt;
+
 /**
  * Accounting management
  *
  * @author Olivier Bitsch
  */
-class PostfixController extends \IgestisController {
+class PostfixController extends \IgestisController
+{
 
     /**
      * Show the list of accountings
      */
-    public function indexAction() {
+    public function indexAction()
+    {
 
-      if (! is_readable(ConfigModuleVars::aliasesFile())) {
-          new \wizz(\Igestis\I18n\Translate::_(sprintf("Warning: the file '%s' is not readable by the webserver", ConfigModuleVars::aliasesFile())), \wizz::$WIZZ_ERROR);
-      } elseif (! is_writable(ConfigModuleVars::aliasesFile())) {
-          new \wizz(\Igestis\I18n\Translate::_(sprintf("Warning: the file '%s' is not writeable by the webserver", ConfigModuleVars::aliasesFile())), \wizz::$WIZZ_WARNING);
-      }
+        if (!is_readable(ConfigModuleVars::aliasesFile())) {
+            new \wizz(\Igestis\I18n\Translate::_(sprintf("Warning: the file '%s' is not readable by the webserver", ConfigModuleVars::aliasesFile())), \wizz::$WIZZ_ERROR);
+        } elseif (!is_writable(ConfigModuleVars::aliasesFile())) {
+            new \wizz(\Igestis\I18n\Translate::_(sprintf("Warning: the file '%s' is not writeable by the webserver", ConfigModuleVars::aliasesFile())), \wizz::$WIZZ_WARNING);
+        }
 
+        if ($this->request->IsPost()) {
 
-      if($this->request->IsPost()) {
+            if (is_array($this->request->getPost("alias"))) {
 
-        if (is_array($this->request->getPost("alias"))) {
-
-            foreach ($this->request->getPost("alias") as $id => $alias) {
-                $user = $this->request->getPost("user");
-                if (isset($user[$id]) && is_array($user[$id])) {
-                    if (! ConfigModuleVars::virtualDomain()) {
-                        $users = $user[$id];
-                    } else {
-                        unset($users);
-                        foreach ($user[$id] as $user) {
-                            $users[] = $user . "@" . ConfigModuleVars::virtualDomain();
+                foreach ($this->request->getPost("alias") as $id => $alias) {
+                    $user = $this->request->getPost("user");
+                    if (isset($user[$id]) && is_array($user[$id])) {
+                        if (!ConfigModuleVars::virtualDomain()) {
+                            $users = $user[$id];
+                        } else {
+                            unset($users);
+                            foreach ($user[$id] as $user) {
+                                $users[] = $user . "@" . ConfigModuleVars::virtualDomain();
+                            }
                         }
+                        $data .= $alias . ": " . implode($users, ", ") . "\n";
                     }
-                    $data .= $alias . ": " . implode($users, ", ") . "\n";
                 }
             }
+            file_put_contents(ConfigModuleVars::aliasesFile(), $data);
+
+            exec("/usr/bin/sudo ../modules/ServerMgmt/bin/helper updateAliases", $message, $returncode);
+            if ($returncode == 0) {
+                new \wizz(\Igestis\I18n\Translate::_(sprintf("The changes have been successfully written to the aliases file", ConfigModuleVars::aliasesFile())), \wizz::$WIZZ_SUCCESS);
+            } else {
+                new \wizz(\Igestis\I18n\Translate::_(sprintf("The changes have been written to the aliases file, but the following error occured: %s", $message)), \wizz::$WIZZ_SUCCESS);
+            }
+
+            $this->redirect(ConfigControllers::createUrl("servermgmt_postfix_index"));
+
         }
-        file_put_contents(ConfigModuleVars::aliasesFile(), $data);
 
+        $rows = file(ConfigModuleVars::aliasesFile(), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $data = array();
 
-        exec("/usr/bin/sudo ../modules/ServerMgmt/bin/helper updateAliases", $message, $returncode);
-        if ($returncode == 0) {
-            new \wizz(\Igestis\I18n\Translate::_(sprintf("The changes have been successfully written to the aliases file", ConfigModuleVars::aliasesFile())), \wizz::$WIZZ_SUCCESS);
-        } else {
-            new \wizz(\Igestis\I18n\Translate::_(sprintf("The changes have been written to the aliases file, but the following error occured: %s", $message)), \wizz::$WIZZ_SUCCESS);
+        foreach ($rows as $line) {
+
+            $line = str_replace(" ", "", $line);
+            $currentLine = explode(':', $line);
+            if (!ConfigModuleVars::virtualDomain()) {
+                $data[$currentLine[0]] = explode(",", $currentLine[1]);
+            } else {
+                foreach (explode(",", $currentLine[1]) as $user) {
+                    $explodedLine = explode("@", $user);
+                    $data[$currentLine[0]][] = $explodedLine[0];
+                }
+            }
+
         }
 
-        $this->redirect(ConfigControllers::createUrl("servermgmt_postfix_index"));
+        $employees = $this->context->entityManager->getRepository("CoreContacts")->getEmployeesList();
 
-
-      }
-
-
-      $rows = file(ConfigModuleVars::aliasesFile(), FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-      $data = array();
-
-      foreach ($rows as $line) {
-
-          $line = str_replace(" ", "", $line);
-          $currentLine = explode(':', $line);
-          if (! ConfigModuleVars::virtualDomain()) {
-              $data[$currentLine[0]] = explode(",", $currentLine[1]);
-          } else {
-              foreach (explode(",", $currentLine[1]) as $user) {
-                  $explodedLine =  explode("@", $user);
-                  $data[$currentLine[0]][] = $explodedLine[0];
-              }
-          }
-
-      }
-
-      $employees = $this->context->entityManager->getRepository("CoreContacts")->getEmployeesList();
-
-      $this->context->render("ServerMgmt/pages/PostfixController.twig", array(
-          'row_data' =>  $data,
-          'employee_list' => $employees
-      ));
+        $this->context->render("ServerMgmt/pages/PostfixController.twig", array(
+            'row_data' => $data,
+            'employee_list' => $employees,
+        ));
 
     }
 }
